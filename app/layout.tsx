147c1c88ba145186afc39357852a5e648b54b533
Octopus-98 <robinzhang1998@gmail.com>
2025-11-11 18:55:45 +0800

    Add Google Analytics (GA4) tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-B8FSBMS5PC"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-B8FSBMS5PC');
+            `,
+          }}
+        />
+      </head>
       <body className={`font-sans antialiased`}>
         {children}
       </body>
